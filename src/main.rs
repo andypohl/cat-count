7e6aec4902ef8ffa7ce149a5af7460be7e50f347
Andy Pohl <andy@pohl.io>
2024-06-13 14:33:38 -0500

    map count is faster

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use clap::Parser;
-use noodles_core::{Position, Region};
+use noodles_core::Region;
 use noodles_fasta as fasta;
 use std::fs::File;
 use std::io;
 use std::io::BufReader;
-//use std::io::Seek;
 use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
@@ -30,45 +29,37 @@ fn path_exists(path: &str) -> Result<PathBuf, String> {
     }
 }
 
+fn get_chrom_list(index_records: &Vec<fasta::fai::Record>) -> Vec<Vec<u8>> {
+    let mut chrom_list = Vec::new();
+    for record in index_records {
+        chrom_list.push(record.name().to_owned());
+    }
+    chrom_list
+}
+
 fn main() -> io::Result<()> {
     let opts = CliOpts::parse();
     let fai_file = File::open(&opts.fai_path)?;
-    let mut index = fasta::fai::Reader::new(BufReader::new(&fai_file));
+    let input_file = File::open(&opts.input_path)?;
+    let mut index = fasta::fai::Reader::new(BufReader::new(fai_file));
     let index_records = index.read_index().unwrap();
-    let input_path = File::open(&opts.input_path)?;
-    //input_path.seek(io::SeekFrom::Start(251492284))?;
-    let mut fasta_reader = fasta::io::Reader::new(BufReader::new(input_path));
-    let start = Position::try_from(1).unwrap();
-    let end = Position::try_from(242696752).unwrap();
-    let chr_2 = fasta_reader
-        .query(&index_records, &Region::new("chr2", start..=end))
-        .unwrap();
-    /*     let mut bgzf_reader = bgzf::Reader::new(input_path);
-    let mut fasta_reader = fasta::io::Reader::new(&mut bgzf_reader);
-    */
-    // 48914580
-
-    let mut g_count = 0;
-    for base in chr_2.sequence().as_ref().iter() {
-        if matches!(*base, b'G' | b'g') {
-            g_count += 1;
-        }
-    }
+    let chrom_list = get_chrom_list(&index_records);
+    let mut fasta_reader = fasta::io::IndexedReader::new(BufReader::new(input_file), index_records);
+    let g_count = chrom_list
+        .iter()
+        .map(|chrom_name| {
+            let chrom = fasta_reader
+                .query(&Region::new(chrom_name.to_owned(), ..))
+                .unwrap();
+            chrom
+                .sequence()
+                .as_ref()
+                .iter()
+                .filter(|&b| matches!(*b, b'G' | b'g'))
+                .count()
+        })
+        .sum::<usize>();
     println!("Number of Gs: {}", g_count);
-
-    //for rec in records {
-    //    match rec {
-    //        Ok(record) => {
-    //            let sequence = record.sequence().as_ref();
-    //            for base in sequence {
-    //                if *base == b'G' || *base == b'g' {
-    //                    g_count += 1;
-    //                }
-    //            }
-    //        }
-    //        Err(e) => eprintln!("Error: {}", e),
-    //    }
-    //}
 
     Ok(())
 }
