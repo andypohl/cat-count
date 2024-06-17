f1347bae28be0481a047cad1699f394009ff2fdf
Andy Pohl <andy@pohl.io>
2024-06-17 17:20:48 -0500

    seek instead of query

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use clap::Parser;
-use noodles_core::Region;
 use noodles_fasta as fasta;
 use noodles_fasta::fai::Record as FaiRecord;
 
@@ -7,25 +6,10 @@ use rayon::prelude::*;
 use std::fs::File;
 use std::io;
 use std::io::BufReader;
+use std::io::Seek;
 use std::path::PathBuf;
 use std::sync::Arc;
 
-trait CustomClone {
-    fn custom_clone(&self) -> Self;
-}
-
-impl CustomClone for FaiRecord {
-    fn custom_clone(&self) -> Self {
-        // Clone each field individually
-        FaiRecord::new(
-            self.name(),
-            self.length(),
-            self.offset(),
-            self.line_bases(),
-            self.line_width(), // Clone other fields as necessary
-        )
-    }
-}
 #[derive(Parser, Debug)]
 struct CliOpts {
     /// File path
@@ -49,26 +33,15 @@ fn path_exists(path: &str) -> Result<PathBuf, String> {
     }
 }
 
-fn get_chrom_list(index_records: &Vec<fasta::fai::Record>) -> Vec<Vec<u8>> {
-    let mut chrom_list = Vec::new();
-    for record in index_records {
-        chrom_list.push(record.name().to_owned());
-    }
-    chrom_list
-}
-
-fn g_count(chrom: Vec<u8>, input_path: Arc<PathBuf>, index_records: Vec<FaiRecord>) -> usize {
-    let input_file = File::open(input_path.as_ref()).unwrap();
-    let mut fasta_reader = fasta::io::IndexedReader::new(BufReader::new(input_file), index_records);
-    let chrom = fasta_reader
-        .query(&Region::new(chrom.to_owned(), ..))
+fn g_count(fai_rec: &FaiRecord, input_path: Arc<PathBuf>) -> usize {
+    let mut buf: Vec<u8> = Vec::new();
+    let mut input_file = File::open(input_path.as_ref()).unwrap();
+    input_file
+        .seek(io::SeekFrom::Start(fai_rec.offset()))
         .unwrap();
-    chrom
-        .sequence()
-        .as_ref()
-        .iter()
-        .filter(|&b| matches!(*b, b'G' | b'g'))
-        .count()
+    let mut fasta_reader = fasta::io::Reader::new(BufReader::new(input_file));
+    fasta_reader.read_sequence(&mut buf).unwrap();
+    buf.iter().filter(|&b| matches!(*b, b'G' | b'g')).count()
 }
 
 fn main() -> io::Result<()> {
@@ -76,17 +49,10 @@ fn main() -> io::Result<()> {
     let fai_file = File::open(&opts.fai_path)?;
     let mut index = fasta::fai::Reader::new(BufReader::new(fai_file));
     let index_records = index.read_index().unwrap();
-    let chrom_list = get_chrom_list(&index_records);
     let input_path = Arc::new(opts.input_path);
-    let g_counted: usize = chrom_list
+    let g_counted: usize = index_records
         .par_iter()
-        .map(|chrom| {
-            g_count(
-                chrom.to_owned(),
-                input_path.clone(),
-                index_records.iter().map(|r| r.custom_clone()).collect(),
-            )
-        })
+        .map(|fai_rec| g_count(&fai_rec, input_path.clone()))
         .sum();
     println!("Number of Gs: {}", g_counted);
 
